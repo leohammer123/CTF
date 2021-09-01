91fe5f63225ed55d50388b9f76ab1f490dd454fb
e04qqq <66953227+leohammer123@users.noreply.github.com>
2021-09-01 19:22:57 +0800

    Create lv1-7.py

---
+++ cryptopals/lv1-7.py
@@ -0,0 +1,15 @@
+# fisrt part
+str1 = 'this is a test'
+str2 = 'wokka wokka!!!'
+
+distance = 0
+for i in range(len(str1)):
+    binary_xor = ord(str1[i])^ord(str2[i])
+    for n in bin(binary_xor)[2:]:
+        if n == str(1):
+            distance += 1
+        
+
+print(f'hamming distance is {distance}')
+
+# seconde part
